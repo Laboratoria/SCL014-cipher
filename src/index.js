b9d9429321fdc0496b50c27d57d8714cc99bb8cf
Sandy-DZ <sandy.d.zarate@gmail.com>
2020-06-15 19:37:58 -0400

    subi el html base y el css

--- src/index.js
+++ src/index.js
@@ -21,6 +21,46 @@ function validar(){
 
 
 
+var enter = document.getElementById("buttonEnter");
+var fisrtScreen = document.getElementById("firstScreen");
+var secondScreen = document.getElementById("secondScreen");
+
+enter.addEventListener("click", function(){
+    fisrtScreen.className = "hideMe";
+    secondScreen.className = "welcomeTo"; 
+  });
+
+
+var siguiente = document.getElementById("buttonNext");
+var newcontactScreen = document.getElementById("newcontactScreen");
+
+siguiente.addEventListener("click", function(){
+  secondScreen.className = "hideMe";
+  newcontactScreen.className = "welcomeTo";
+});
+
+
+
+var contactScreen = document.getElementById("contactScreen");
+var botonListo = document.getElementById("buttonListo");
+
+botonListo.addEventListener("click", function(){
+
+newcontactScreen.className ="hideMe";
+  contactScreen.className = "welcomeTo";
+
+  var userName = document.getElementById("userName").value;
+  var firstContact = document.getElementById("contactName").value;
+ document.getElementById("allContacts").innerHTML = "Hola " + userName + ", en caso de emergencia enviaremos el mensaje a tus contactos favoritos"; 
+document.getElementById("ContactOne").innerText = firstContact;
+});
+
+
+var addContact = document.getElementById("buttonadd");
+addContact.addEventListener("click", function(){
+  newcontactScreen.className ="welcomeTo";
+}
+)
 
 
 
