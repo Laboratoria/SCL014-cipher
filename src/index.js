929d5a4160da6408a4d21548e370dfdfec5fcaba
Sandy-DZ <sandy.d.zarate@gmail.com>
2020-06-22 05:49:56 -0400

    se creo la función de limpiar consolas

--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
 import cipher from './cipher.js';
 
-//console.log(cipher);
-
-//let txtInt = document.getElementById("originalText").value;
-//let offNum = document.getElementById("desplazamiento").value;
-// let txtCipher = document.getElementById("textoCifrado");
+//console.log(cipher.encode(), cipher.decode());
 
 // <----- separación de las variables del cipher---------> 
 
-let enter = document.getElementById("buttonEnter"); // boton de inicio Entrar
+let enter = document.getElementById("buttonEnter"); 
 let fisrtScreen = document.getElementById("firstScreen");
 let nextBtn = document.getElementById("buttonNext");
-let newcontactScreen = document.getElementById("newcontactScreen");
 let contactList = document.getElementById("contactLog");
 let botonListo = document.getElementById("buttonListo");
-let goBackMsg = document.getElementById("goBack");
 let finalScreen = document.getElementById("msgSent");
-let returnMsgPage = document.getElementById("buttonLast");
-let fakeCal = document.getElementById("calendarLog");
-let backToMsg = document.getElementById("goBackTwo");
+let goBackMsg = document.getElementById("goBack");
+let exitBtn = document.getElementById("btnClose");
+let instructionsBtn = document.getElementById("btnInstructions");
+let clearContent = document.getElementById("clearContent");
+
+// Funcion de limpiar la consola
+
+  clearContent.addEventListener("click" , function (){
+    document.getElementById("originalText").value = " ";
+    document.getElementById("textoCifrado").value = " ";
+    
+  });
+  
+
 
 // funcion de cifrar
 document.getElementById("cifrarTexto").addEventListener('click', () => {
@@ -80,30 +85,31 @@ goBackMsg.addEventListener("click", function () {
 
 //Funciones del Popup
 //Botón al abrir las instrucciones
-let instructionsBtn = document.getElementById("btnInstructions");
 instructionsBtn.addEventListener("click", function () {
   document.getElementById("instructions").style.display = "block";
 });
 
 //Botón para cerrar las instrucciones
-let exitBtn = document.getElementById("btnClose");
 exitBtn.addEventListener("click", function () {
   document.getElementById("instructions").style.display = "none";
 });
 
 //Volver a la pantalla de mensajes
+let returnMsgPage = document.getElementById("buttonLast");
 returnMsgPage.addEventListener("click", function () {
   msgSent.className = "hideMe";
   secondScreen.className = "welcomeTo";
 });
 
 //Abrir el calendario falso
+let fakeCal = document.getElementById("calendarLog");
 fakeCal.addEventListener("click", function () {
   secondScreen.className = "hideMe";
   fakeCalendar.className = "welcomeTo";
 });
 
 //Volver a la pantalla de mensajes
+let backToMsg = document.getElementById("goBackTwo");
 backToMsg.addEventListener("click", function () {
   fakeCalendar.className = "hideMe";
   secondScreen.className = "welcomeTo";
